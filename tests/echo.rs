694585ec8b1a4fbdaf135c00b070cf7944a59df4
Raman Hafiyatulin <r.gafiyatullin@me.com>
2025-07-16 13:50:24 +0300

    feat: an example with request-response

--- tests/echo.rs
+++ tests/echo.rs
@@ -10,6 +10,9 @@ pub mod proto {
     use serde_json::Value;
 
     #[message]
+    pub struct Hey;
+
+    #[message]
     pub struct V(pub Value);
 
     #[message(ret = Value)]
@@ -19,11 +22,18 @@ pub mod proto {
 pub mod echo {
     use crate::proto;
     use elfo::{msg, ActorGroup, Blueprint, Context};
+    use serde_json::json;
 
     pub async fn actor(mut ctx: Context) {
         while let Some(envelope) = ctx.recv().await {
             let sender = envelope.sender();
             msg!(match envelope {
+                proto::Hey => {
+                    ctx.request_to(sender, proto::R(json!("hello!")))
+                        .resolve()
+                        .await
+                        .expect("oh :(");
+                }
                 v @ proto::V => {
                     let _ = ctx.send_to(sender, v).await;
                 }
@@ -49,6 +59,11 @@ async fn marshalling() {
     run_scenario(include_str!("echo/marshalling.yaml")).await;
 }
 
+#[tokio::test]
+async fn request_response() {
+    run_scenario(include_str!("echo/request-response.yaml")).await;
+}
+
 async fn run_scenario(scenario_text: &str) {
     let _ = tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
@@ -58,7 +73,8 @@ async fn run_scenario(scenario_text: &str) {
 
     let marshalling = MarshallingRegistry::new()
         .with(Regular::<crate::proto::V>)
-        .with(Request::<crate::proto::R>);
+        .with(Request::<crate::proto::R>)
+        .with(Regular::<crate::proto::Hey>);
     let scenario: Scenario = serde_yaml::from_str(scenario_text).unwrap();
     let exec_graph = Executable::build(marshalling, &scenario).expect("building graph");
     let report = exec_graph
