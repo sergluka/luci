f1bfa93cf7f90d61a3c2896e96406414c16a8250
Sergey Lukashevich <slukashevich@bhft.com>
2025-07-17 15:14:07 +0300

    add dot_writer

--- src/scenario/visualization.rs
+++ src/scenario/visualization.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fmt::Display};
+
+use dot_writer::{Attributes, DotWriter, Scope};
 
 use crate::{
     scenario::DefEventKind,
@@ -9,8 +11,13 @@ use super::{DefEvent, Scenario};
 
 impl Draw<Scenario> for DiGraphDrawer {
     fn draw(&self, item: &Scenario) -> String {
-        let mut acc = String::new();
-        acc.push_str("digraph test { rankdir=LR layout=dot\n");
+        let mut output_bytes = Vec::new();
+
+        let mut writer = DotWriter::from(&mut output_bytes);
+        writer.set_pretty_print(true);
+
+        let mut digraph = writer.digraph();
+        digraph.set_rank_direction(dot_writer::RankDirection::LeftRight);
 
         let mut seen_ids = HashSet::new();
         for event in item
@@ -20,22 +27,28 @@ impl Draw<Scenario> for DiGraphDrawer {
             .cloned()
             .collect::<Vec<DefEvent>>()
         {
-            acc.push_str(&format!("  {}", render_node(&event)));
-            acc.push('\n');
+            draw_node(&mut digraph, &event);
         }
 
         for event in &item.events {
             for subnode_name in &event.prerequisites {
-                acc.push_str(&format!("  \"{}\" -> \"{}\"\n", subnode_name, event.id));
+                digraph.edge(quote(subnode_name), quote(&event.id));
             }
         }
 
-        acc.push_str("\n}\n");
-        acc
+        drop(digraph);
+
+        String::from_utf8(output_bytes).unwrap()
     }
 }
 
-fn render_node(event: &DefEvent) -> String {
+fn quote(str: &impl Display) -> String {
+    format!("\"{}\"", str.to_string())
+}
+
+fn draw_node(digraph: &mut Scope, event: &DefEvent) {
+    let mut node = digraph.node_named(quote(&event.id));
+
     let (kind, data) = match &event.kind {
         DefEventKind::Bind(bind) => ("BIND", serde_yaml::to_string(&bind).unwrap()),
         DefEventKind::Recv(recv) => ("RECV", serde_yaml::to_string(&recv).unwrap()),
@@ -44,8 +57,6 @@ fn render_node(event: &DefEvent) -> String {
         DefEventKind::Delay(delay) => ("DELAY", serde_yaml::to_string(&delay).unwrap()),
     };
 
-    format!(
-        r#""{}" [label="{}\nid={}\n\n{}"]"#,
-        event.id, kind, event.id, data,
-    )
+    let label = format!(r#"{}\nid={}\n\n{}"#, kind, event.id, data);
+    node.set_label(&label);
 }
